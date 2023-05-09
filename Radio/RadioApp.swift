320cf8c32957805ee7e94d2cdc0d7cc1b9b87fba
Kyle Erhabor <kyleerhabor@gmail.com>
2023-05-08 20:24:57 -0400

    Initial Commit

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by Kyle Erhabor on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
