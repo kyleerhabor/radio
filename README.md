1dccabdb5c5a290d68bfd7608820f483a71bd6f9
Kyle Erhabor <kyleerhabor@gmail.com>
2023-05-12 14:55:26 -0400

    Add more user info

--- README.md
+++ README.md
@@ -7,3 +7,26 @@ Broadcast your currently playing song in [Doppler](https://brushedtype.co/dopple
   
   <img src="Documentation/example.png">
 </details>
+
+## Installing
+
+You can either download one of the [releases](https://github.com/KyleErhabor/Radio/releases) or build from source in Xcode.
+
+## Limitations
+
+### Discord Client
+
+Currently, Radio expects that Discord is running throughout its entire execute. This has some unfortunate consequences:
+- If you quit Discord while Radio is running, it'll begin using excess resources.
+- If you start Radio before Discord has started or initialized, it won't be able to send rich presence.
+
+In both these cases, you should quit Radio and start it after Discord.
+
+### Artwork
+
+Discord uses Activity Asset Images for images displayed in Rich Presence. These must either be URLs to images on the web or IDs for asset images uploaded to the Developer Portal. This is troublesome, as album artwork in Doppler is image data and does not correspond to any URL. To circumvent this limitation, Radio broadcasts an ID associated with the song album, where the album artist and album name are separated by a space and normalized. For example, "Rocket" by Susumu Hirasawa is broadcasted as `susumu_hirasawa_rocket`. With this composition, an image associated with the album can be uploaded to the Developer Portal.
+1. Go to the [Developer Portal](https://discord.com/developers/applications).
+2. Click the "New Application" button to create a new application for yourself (e.g. call it Radio).
+3. On the General Information tab, copy the Application ID and replace the default used in Radio with it.
+4. Click on the Rich Presence > Art Assets tabs and use the "Add Image(s)" button to upload album artwork. Make sure the names of the image files uploaded match with the ID composition.
+5. Enable "Display Artwork" in Radio for it to send IDs to Discord. With that, you should start seeing album artwork. 
--- Radio/ContentView.swift
+++ Radio/ContentView.swift
@@ -16,9 +16,19 @@ struct ContentView: View {
 
   @State private var clientId: String = ""
 
+  let formatter: DateComponentsFormatter = {
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = .second
+    formatter.unitsStyle = .full
+
+    return formatter
+  }()
+
   var body: some View {
-    VStack {
+    Form {
       HStack {
+        Spacer()
+
         Text("Radio")
           .bold()
 
@@ -27,46 +37,46 @@ struct ContentView: View {
 
         Text("Broadcast your currently playing song in Doppler to Discord.")
           .font(.title)
+
+        Spacer()
       }.font(.largeTitle)
 
-      VStack(alignment: .leading) {
-        LabeledContent("Client ID") { // TODO: Add details about the relation to displaying artwork.
-          TextField("Client ID", text: $clientId, prompt: Text(defaultClientId))
-            .textFieldStyle(.plain)
-            .onChange(of: clientId) { id in
-              guard !id.isEmpty else {
-                appClientId = defaultClientId
+      // TODO: Add details about the relation to displaying artwork.
+      // TODO: Figure out how to monospace the value but not the label.
+      TextField("Client ID", text: $clientId, prompt: Text(defaultClientId))
+        .onChange(of: clientId) { id in
+          guard !id.isEmpty else {
+            appClientId = defaultClientId
 
-                return
-              }
-
-              appClientId = id
-            }
-        }
-
-        HStack {
-          Slider(value: $refreshRate, in: 1...10, step: 1) {
-            Text("Refresh Rate")
+            return
           }
 
-          Text("\(refreshRate, format: .number) seconds")
+          appClientId = id
         }
 
-        Toggle(isOn: $displayArtwork) {
-          Text("Display Artwork")
+      HStack {
+        Slider(value: $refreshRate, in: 1...10, step: 1) {
+          Text("Refresh Rate")
+        }
 
-          VStack(alignment: .leading, spacing: 8) {
-            Text("You'll need to create your own Discord application and Rich Presence Assets for each artwork. The asset keys should be the album artist and album name separated by a space, which will be converted by Discord.")
-
-            Text("e.g. ")
-            + Text("Susumu Hirasawa error CD").fontDesign(.monospaced)
-            + Text(" -> ").bold()
-            + Text("susumu_hirasawa_error_cd").fontDesign(.monospaced)
-          }
+        if let s = formatter.string(from: refreshRate) {
+          Text(s)
         }
       }
+
+      Toggle(isOn: $displayArtwork) {
+        Text("Display Artwork")
+
+        Text("You'll need to create your own Discord application and Rich Presence Assets for each artwork. The asset keys should be the album artist and album name separated by a space, which will be converted by Discord.")
+
+        Text("e.g. ")
+        + Text("Susumu Hirasawa error CD").fontDesign(.monospaced)
+        + Text(" -> ").bold()
+        + Text("susumu_hirasawa_error_cd").fontDesign(.monospaced)
+      }
     }
-    .padding()
+    .formStyle(.grouped)
+    .frame(minWidth: 384, minHeight: 256)
     .onAppear {
       clientId = appClientId
     }
